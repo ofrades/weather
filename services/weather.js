afb6af2b3ed989c3a312c835ef19a2ee5cc9930f
Miguel Bastos <mig.silva@gmail.com>
2021-04-24 01:57:06 +0100

    feat: add new service to get city by name

--- services/weather.js
+++ services/weather.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
-export const getWeather = async (lat, lon) => {
+export const getLocation = async (lat, lon) => {
   try {
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCity = async (city) => {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
     );
     return response.data;
   } catch (error) {
