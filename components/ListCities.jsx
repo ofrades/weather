de04fd489325220d3d7bf7a344db10a2d89ee4de
Miguel Bastos <mig.silva@gmail.com>
2021-04-25 19:26:50 +0100

    refactor: do not need layout and break weather

---
+++ components/ListCities.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { List, AddButton, RemoveButton, Loading } from "./styles";
+
+const ListCities = ({ arrCities, setArrCities, setSearchQuery }) => {
+  const showCity = (item) => {
+    setSearchQuery(item);
+  };
+
+  const removeCity = (item) => {
+    setArrCities(arrCities.filter((city) => city !== item));
+  };
+
+  return (
+    <List>
+      {arrCities.map((item) => (
+        <li key={item}>
+          <AddButton onClick={() => showCity(item)}>{item}</AddButton>
+          <RemoveButton onClick={() => removeCity(item)}>Remove</RemoveButton>
+        </li>
+      ))}
+    </List>
+  );
+};
+
+export default ListCities;
---
+++ components/Search.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { InputContainer, Input, AddCity, Loading } from "./styles";
+
+const Search = ({ setArrCities, setSearchQuery }) => {
+  const [cityFromInput, setCityFromInput] = useState();
+
+  const handleInput = (e) => {
+    setCityFromInput(e.target.value);
+  };
+
+  const addCity = () => {
+    setArrCities((oldArr) => [...oldArr, cityFromInput]);
+    setSearchQuery(cityFromInput);
+  };
+
+  return (
+    <InputContainer>
+      <Input placeholder="Add Cities" onChange={handleInput} />
+      <AddCity onClick={() => addCity()}>
+        <a>+</a>
+      </AddCity>
+    </InputContainer>
+  );
+};
+
+export default Search;
