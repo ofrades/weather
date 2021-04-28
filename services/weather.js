730e26d4f5938a7b0c54cbcf5391ea8fd13d5588
Miguel Bastos <mig.silva@gmail.com>
2021-04-28 09:31:55 +0100

    fix: http call to https

--- services/weather.js
+++ services/weather.js
@@ -19,7 +19,7 @@ export const getCity = async (city = "Leiria", lat, lon) => {
   try {
     if (city === "Your Location" || city === undefined) {
       const reverseLocation = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${process.env.NEXT_PUBLIC_API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${process.env.NEXT_PUBLIC_API_KEY}`
       );
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${reverseLocation.data[0].name}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
